041f2df3555765cd156661b5ed4d6e1bb6bb854d
toppye <muiznero@gmail.com>
2025-04-17 14:54:58 +0100

    embedded repo added

---
+++ src/main/java/com/muiz/doggat/DoggatApplication.java
@@ -0,0 +1,13 @@
+package com.muiz.doggat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muiz/doggat/DoggatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muiz.doggat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
