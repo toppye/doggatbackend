9c124f9686df3877af2c75c703102d259526b1d2
toppye <muiznero@gmail.com>
2025-04-17 16:03:17 +0100

    bean injection tested var ctx was use to hold return from main controller

--- src/main/java/com/muiz/doggat/DoggatApplication.java
+++ src/main/java/com/muiz/doggat/DoggatApplication.java
@@ -2,15 +2,20 @@ package com.muiz.doggat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DoggatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoggatApplication.class, args);
+		var ctx = SpringApplication.run(DoggatApplication.class, args);
 
-		MyFirstClass myFirstClass = new MyFirstClass();
-		 System.out.println(myFirstClass.SayHello());
+		MyFirstClass myFirstClass = ctx.getBean(MyFirstClass.class);
+		System.out.println(myFirstClass.SayHello());
 	}
 
+	@Bean
+	public MyFirstClass myFirstClass() {
+		return new MyFirstClass();
+	}
 }
