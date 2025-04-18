47c0068c04dddd7e340a1119076c09edcfcce3e7
toppye <muiznero@gmail.com>
2025-04-18 10:23:34 +0100

    bean injection with same class but differen instantiation

--- src/main/java/com/muiz/doggat/ApplicationConfig.java
+++ src/main/java/com/muiz/doggat/ApplicationConfig.java
@@ -1,12 +1,20 @@
 package com.muiz.doggat;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ApplicationConfig {
     @Bean
     public FirstClassTest firstClassTest() {
         return new FirstClassTest("First beans or 12345");
+    }
+
+    @Bean
+    @Primary
+    public FirstClassTest secondClassTest() {
+        return new FirstClassTest("my second beans");
     }
 }
--- src/main/java/com/muiz/doggat/MyFirstService.java
+++ src/main/java/com/muiz/doggat/MyFirstService.java
@@ -1,5 +1,6 @@
 package com.muiz.doggat;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
